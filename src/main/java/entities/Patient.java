4407d88a55b6ae56c0aba630a99c30fa681de236
Yazid <yazidbougrine99@gmail.com>
2022-01-20 21:20:39 +0100

    test

--- src/main/java/entities/Patient.java
+++ src/main/java/entities/Patient.java
@@ -16,42 +16,42 @@ import javax.persistence.GenerationType;
 
 @Entity @Table(name = "Patients")
 public class Patient implements Serializable {
-	
-	
-	
+
+
+
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idPatient;
 	private String namePatient;
 	private String emailPatient;
 	private String countryPatient;
-public Patient() {
+	public Patient() {
 		super();
 	}
 
 	public Patient(String namePatient, String emailPatient, String countryPatient) {
-	super();
-	this.namePatient = namePatient;
-	this.emailPatient = emailPatient;
-	this.countryPatient = countryPatient;
-}
+		super();
+		this.namePatient = namePatient;
+		this.emailPatient = emailPatient;
+		this.countryPatient = countryPatient;
+	}
 
 	public Patient(int idPatient, String namePatient, String emailPatient, String countryPatient) {
-	super();
-	this.idPatient = idPatient;
-	this.namePatient = namePatient;
-	this.emailPatient = emailPatient;
-	this.countryPatient = countryPatient;
-}
+		super();
+		this.idPatient = idPatient;
+		this.namePatient = namePatient;
+		this.emailPatient = emailPatient;
+		this.countryPatient = countryPatient;
+	}
 
-	//	@ManyToOne
+//	@ManyToOne
 //	@JoinTable( name = "PATIENT_MEDS",
-//    joinColumns = @JoinColumn( name = "idPatient" ),
-//    inverseJoinColumns = @JoinColumn( name = "idMed" ) )
-	//private List<Meds> meds =new ArrayList<>();
+//	joinColumns = @JoinColumn( name = "idPatient" ),
+//	inverseJoinColumns = @JoinColumn( name = "idMed" ) )
+//	private List<Meds> meds =new ArrayList<>();
 	public int getIdPatient() {
 		return idPatient;
 	}
-	
+
 	public void setIdPatient(int idPatient) {
 		this.idPatient = idPatient;
 	}
@@ -73,31 +73,31 @@ public Patient() {
 	public void setCountryPatient(String countryPatient) {
 		this.countryPatient = countryPatient;
 	}
-//	public void setMeds(List<Meds> meds) {
-//		this.meds = meds;
-//	}
-//	@Override
-//	public String toString() {
-//		return "Patient [idPatient=" + idPatient + ", namePatient=" + namePatient + ", emailPatient=" + emailPatient
-//				+ ", countryPatient=" + countryPatient + ", meds=" + meds + "]";
-//	}
-//	@Override
-//	public int hashCode() {
-//		return Objects.hash(countryPatient, emailPatient, idPatient, meds, namePatient);
-//	}
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		Patient other = (Patient) obj;
-//		return Objects.equals(countryPatient, other.countryPatient) && Objects.equals(emailPatient, other.emailPatient)
-//				&& idPatient == other.idPatient && Objects.equals(meds, other.meds)
-//				&& Objects.equals(namePatient, other.namePatient);
-//	}
+	//	public void setMeds(List<Meds> meds) {
+	//		this.meds = meds;
+	//	}
+	//	@Override
+	//	public String toString() {
+	//		return "Patient [idPatient=" + idPatient + ", namePatient=" + namePatient + ", emailPatient=" + emailPatient
+	//				+ ", countryPatient=" + countryPatient + ", meds=" + meds + "]";
+	//	}
+	//	@Override
+	//	public int hashCode() {
+	//		return Objects.hash(countryPatient, emailPatient, idPatient, meds, namePatient);
+	//	}
+	//	@Override
+	//	public boolean equals(Object obj) {
+	//		if (this == obj)
+	//			return true;
+	//		if (obj == null)
+	//			return false;
+	//		if (getClass() != obj.getClass())
+	//			return false;
+	//		Patient other = (Patient) obj;
+	//		return Objects.equals(countryPatient, other.countryPatient) && Objects.equals(emailPatient, other.emailPatient)
+	//				&& idPatient == other.idPatient && Objects.equals(meds, other.meds)
+	//				&& Objects.equals(namePatient, other.namePatient);
+	//	}
 
 	@Override
 	public int hashCode() {
@@ -116,9 +116,9 @@ public Patient() {
 		return Objects.equals(countryPatient, other.countryPatient) && Objects.equals(emailPatient, other.emailPatient)
 				&& idPatient == other.idPatient && Objects.equals(namePatient, other.namePatient);
 	}
-	
-	
-	
 
-	
+
+
+
+
 }
--- src/main/webapp/listPatient.jsp
+++ src/main/webapp/listPatient.jsp
@@ -10,7 +10,7 @@
 <body>
 
             <header>
-                <nav class="navbar navbar-expand-md navbar-dark" style="background-color: red">
+                <nav class="navbar navbar-expand-md navbar-dark" style="background-color: darkgrey">
                     <div>
                         <a  class="navbar-brand"> Management</a>
                     </div>
